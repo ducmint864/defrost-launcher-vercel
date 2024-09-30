37e3861461962cce33f4d3065751516d094b289a
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2024-09-30 22:45:01 +0700

    First commit

---
+++ src/app/hero.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Button, Typography } from "@material-tailwind/react";
+import bg from "../../public/images/bg1.jpg";
+import { Footer } from "@/components";
+function Hero() {
+  return (
+    <div
+      className="relative min-h-screen w-full bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${bg.src})` }}
+    >
+      <div className="absolute inset-0 h-full w-full bg-gray-900/60" />
+      <div className="grid min-h-screen px-8">
+        <div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
+          <Typography variant="h3" color="white" className="mb-2">
+            Project Launch Pad
+          </Typography>
+          <Typography variant="h1" color="white" className="lg:max-w-3xl">
+            Polkadot Hackathon - DeFi Track
+          </Typography>
+          <Typography
+            variant="lead"
+            color="white"
+            className="mt-1 mb-12 w-full md:max-w-full lg:max-w-2xl"
+          >
+            Our launchpad allows projects to raise funds by locking vTokens for
+            staking rewards, with native tokens in return and automatic vToken
+            release post lock-up.{" "}
+          </Typography>
+          <div className="flex items-center gap-4">
+            <Button variant="gradient" color="white">
+              Lauchpad
+            </Button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/components/footer.tsx
@@ -0,0 +1,20 @@
+import { Typography } from "@material-tailwind/react";
+
+const CURRENT_YEAR = new Date().getFullYear();
+
+export function Footer() {
+  return (
+    <footer className="pb-5 p-10 md:pt-10">
+      <div className="container flex flex-col mx-auto">
+        <Typography
+          color="blue-gray"
+          className="text-center mt-1 font-normal !text-white"
+        >
+          &copy; {CURRENT_YEAR} Made with Solidithi Squad
+        </Typography>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
