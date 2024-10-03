ed088cc5d9d693433a2d71ad2bd1ba56c36b07d1
ducmint864 <nddminh2021@gmail.com>
2024-10-03 13:35:14 +0700

    remove old wallet-connect code

--- global.d.ts
+++
@@ -1,7 +0,0 @@
-interface Window {
-    ethereum?: {
-      isMetaMask?: boolean;
-      request?: (args: { method: string; params?: any[] }) => Promise<any>;
-      on?: (eventName: string, callback: (...args: any[]) => void) => void;
-    };
-  }
