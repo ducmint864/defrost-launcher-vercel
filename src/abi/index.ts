8a11e070f38b9240cdcced9db87da03f5a8ec9e8
ducmint864 <nddminh2021@gmail.com>
2024-10-18 17:19:13 +0700

    add barrel index files to export abis, custom thirdweb chain specs, config filefs

---
+++ src/abi/index.ts
@@ -0,0 +1,13 @@
+import ProjectPoolFactoryArtifacts from "./ProjectPoolFactory.json";
+import ProjectPoolArtifacts from "./ProjectPool.json"
+
+const ProjectPoolFactoryABI = ProjectPoolFactoryArtifacts.abi;
+const ProjectPoolABI = ProjectPoolArtifacts.abi;
+
+export {
+    ProjectPoolFactoryArtifacts,
+    ProjectPoolFactoryABI,
+    ProjectPoolArtifacts,
+    ProjectPoolABI,
+}
+
