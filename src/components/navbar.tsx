2c6e0df8bc8dc556c6f1cf7ec9d0dc141d7a19c5
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2024-10-19 22:55:04 +0700

    fix navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import {
-  Navbar as MTNavbar,
-  Collapse,
-  Button,
-  IconButton,
-  Typography,
-} from "@material-tailwind/react";
-import {
   RectangleStackIcon,
   CommandLineIcon,
   XMarkIcon,
@@ -26,13 +19,7 @@ function NavItem({ children, href }: NavItemProps) {
   return (
     <li>
       <Link href={href || "#"} passHref>
-        <Typography
-          as="span"
-          variant="paragraph"
-          className="flex items-center gap-2 font-medium"
-        >
-          {children}
-        </Typography>
+        <span className="flex items-center gap-2 font-medium">{children}</span>
       </Link>
     </li>
   );
@@ -65,8 +52,8 @@ export function Navbar({
     },
   ]);
 
-  const [open, setOpen] = React.useState(false);
-  const [isScrolling, setIsScrolling] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [isScrolling, setIsScrolling] = useState(false);
 
   const handleOpen = () => setOpen((cur) => !cur);
 
@@ -86,16 +73,16 @@ export function Navbar({
     };
     checkProjectOwner();
   }, []);
-  React.useEffect(() => {
+
+  useEffect(() => {
     window.addEventListener(
       "resize",
       () => window.innerWidth >= 960 && setOpen(false)
     );
   }, []);
 
-  React.useEffect(() => {
+  useEffect(() => {
     function handleScroll() {
-      // Cập nhật trạng thái khi cuộn
       if (window.scrollY > 0) {
         setIsScrolling(true);
       } else {
@@ -103,30 +90,27 @@ export function Navbar({
       }
     }
 
+    // Đăng ký sự kiện scroll khi component được mount
     window.addEventListener("scroll", handleScroll);
+
+    // Cleanup sự kiện khi component bị unmount
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <MTNavbar
-      shadow={false}
-      fullWidth
-      blurred={false}
-      color="transparent"
-      className={`fixed top-0 z-50 border-0 transition-all duration-300 ${
-        isScrolling ? "bg-[#0b162d] bg-opacity-100" : ""
+    <div
+      className={`fixed top-0 z-50 w-full transition-all duration-300 ${
+        isScrolling ? "bg-[#0b162d] bg-opacity-100" : "bg-transparent"
       }`}
+      // style={{ backgroundColor }}
     >
-      <div className="container mx-auto flex items-center justify-between">
+      <div className="container mx-auto flex items-center justify-between py-2 ">
         <Link href="/" passHref>
-          <Typography
-            color="white"
-            className="text-lg font-bold transition-transform transform hover:-translate-y-1 duration-300"
-          >
-            Solidithi Launchpad
-          </Typography>
+          <span className="text-lg font-bold text-white inline-block transition-transform transform hover:-translate-y-1 duration-300">
+            Defrost Launcher
+          </span>
         </Link>
-        <ul className="ml-10 hidden items-center gap-6 lg:flex text-white ">
+        <ul className="ml-10 hidden items-center gap-6 lg:flex text-white">
           {NAV_MENU.map(({ name, icon: Icon, href }) => (
             <NavItem key={name} href={href}>
               <div className="flex items-center transition-transform transform hover:-translate-y-1 duration-300">
@@ -139,20 +123,18 @@ export function Navbar({
         <div className="hidden items-center gap-4 lg:flex">
           <ConnectWallet />
         </div>
-        <IconButton
-          variant="text"
-          color="white"
+        <button
           onClick={handleOpen}
-          className="ml-auto inline-block lg:hidden"
+          className="ml-auto inline-block lg:hidden text-white"
         >
           {open ? (
             <XMarkIcon strokeWidth={2} className="h-6 w-6" />
           ) : (
             <Bars3Icon strokeWidth={2} className="h-6 w-6" />
           )}
-        </IconButton>
+        </button>
       </div>
-      <Collapse open={open}>
+      {open && (
         <div className="container mx-auto mt-4 rounded-lg bg-white px-6 py-5">
           <ul className="flex flex-col gap-4 text-gray-900">
             {NAV_MENU.map(({ name, icon: Icon, href }) => (
@@ -163,8 +145,8 @@ export function Navbar({
             ))}
           </ul>
         </div>
-      </Collapse>
-    </MTNavbar>
+      )}
+    </div>
   );
 }
 
