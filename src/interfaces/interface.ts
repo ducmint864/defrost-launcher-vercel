b352741776c8dddd0a55f398b742fc5ad0c5b5a3
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2024-10-25 20:22:31 +0700

    hihi

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,25 +1,27 @@
-import { Project } from "@prisma/client";
+import { Launchpad, Project } from "@prisma/client";
 
 export type ProjectStatus = "ended" | "upcoming" | "pending";
 
 export interface DBProject extends Project {
-    // id: number;
-    // projectID: string;
-    // projectName: string;
-    // projectOwnerAddress: string;
-    // description: string;
-    // shortDescription: string;
-    // projectImageUrls: string[];
-    // txnHashCreated: string;
-    // projectTitle: string;
-    // projectLogoImageUrl: string[];
-    // endDate: Date;
-    // startDate: Date;
-    // status: Status;
-    raisedAmount?: string; // bigint as string
-    isProjectSoftcapReached?: boolean;
-    isProjectFullyToppedUp?: boolean;
-    status?: ProjectStatus;
-    hardCap?: string;
-    softCap?: string;
+  // id: number;
+  // projectID: string;
+  // projectName: string;
+  // projectOwnerAddress: string;
+  // description: string;
+  // shortDescription: string;
+  // projectImageUrls: string[];
+  // txnHashCreated: string;
+  // projectTitle: string;
+  // projectLogoImageUrl: string[];
+  // endDate: Date;
+  // startDate: Date;
+  // status: Status;
+  raisedAmount?: string; // bigint as string
+  isProjectSoftcapReached?: boolean;
+  isProjectFullyToppedUp?: boolean;
+  status?: ProjectStatus;
+  hardCap?: string;
+  softCap?: string;
 }
+
+export interface DBLaunchpad extends Launchpad {}
